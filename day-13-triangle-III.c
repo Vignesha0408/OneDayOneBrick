8d7fbb608c1370662f178d0b0747f2d7e6ee4fcc
Vignesha U G <143516355+Vignesha0408@users.noreply.github.com>
2026-02-03 01:41:45 +0530

    Add files via upload

---
+++ day-13-triangle-III.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    for(int i=0;i<6;i++)
+    {for(int j=0;j<6;j++)
+        if(i==j||i>j)
+        printf("*");
+        else
+        printf(" ");
+    printf("\n");
+    }
+
+}
+/*
+*     
+**
+***
+****
+*****
+*******/
